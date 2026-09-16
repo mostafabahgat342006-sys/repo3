92f642aaff439ad833515f854935c5de3c770a9a
mostafa bahgat <mostafabahgat342006@gmail.com>
2026-09-16 08:23:55 +0300

    Q1

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ internal class Program
 {
     static void Main(string[] args)
     {
+        // array start from index 0 
+        double [] prices = { 25.5, 40.0 , 33.75 };
+        Console.WriteLine(prices[1]);
 
 
 
@@ -29,6 +32,6 @@ internal class Program
 
 
 
-       
+
     }
 }
