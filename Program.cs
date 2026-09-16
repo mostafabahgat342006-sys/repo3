92ff93e973b843a0c5eeb61161f9363bb836de50
mostafa bahgat <mostafabahgat342006@gmail.com>
2026-09-16 08:59:02 +0300

    Q9

--- Program.cs
+++ Program.cs
@@ -91,6 +91,27 @@ internal class Program
 
         Console.WriteLine("----------------------------------");
 
+        static bool TryGetPrice(string title, out double price)
+        {
+            if (title == "Clean Code")
+            {
+                price = 25.5;
+                return true;
+            }
+            else
+            {
+                price = 0;
+                return false;
+            }
+        }
+        double price;
+
+        if ( TryGetPrice("Clean Code", out price) )
+        {
+            Console.WriteLine(price);
+        }
+
+        Console.WriteLine("----------------------------------");
 
 
 
